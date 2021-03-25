029c4f9f329d9ed298695d2a2c5f4b11ce26a6b9
EasonC13 <pricean01@gmail.com>
2021-03-25 15:16:55 +0800

    add config example

---
+++ app/core/config_example.py
@@ -0,0 +1,46 @@
+import os
+
+from dotenv import load_dotenv
+from starlette.datastructures import CommaSeparatedStrings, Secret
+from databases import DatabaseURL
+
+#API_V1_STR = "/api"
+
+#JWT_TOKEN_PREFIX = "Token"
+#ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
+
+load_dotenv(".env")
+
+MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
+MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
+#SECRET_KEY = Secret(os.getenv("SECRET_KEY", "secret key for project"))
+
+PROJECT_NAME = os.getenv("PROJECT_NAME", "AI_Chatbot_Platform")
+ALLOWED_HOSTS = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", "localhost"))
+
+API_PORT = 13525
+API_HOST = "0.0.0.0"
+API_WORKER = 5
+
+DEBUG = False
+
+MONGODB_URL = os.getenv("MONGODB_URL", "")  # deploying without docker-compose
+
+
+#chage to yours
+MONGODB_PORT = 27017
+MONGODB_HOST = "localhost"
+
+MONGODB_USERNAME = os.getenv("USER", "user")
+MONGODB_PASSWORD = os.getenv("MONGO_PASSWORD", f"mongo_{MONGODB_USERNAME}")
+
+if not MONGODB_URL:
+    MONGODB_URL = DatabaseURL(f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}")
+else:
+    MONGODB_URL = DatabaseURL(MONGODB_URL)
+
+DATABASE_NAME = "ai_chatbot_platform"
+COLLECTION_Bots = "bots"
+
+
+Model_Generate_URL = ["http://localhost:13510/"]
