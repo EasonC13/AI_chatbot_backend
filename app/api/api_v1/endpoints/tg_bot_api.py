2f674e6f2dc9890bd503f55c5cf521ffbb06c811
EasonC13 <pricean01@gmail.com>
2021-03-29 07:37:55 +0800

    add bot to database

--- app/api/api_v1/endpoints/tg_bot_api.py
+++ app/api/api_v1/endpoints/tg_bot_api.py
@@ -1,10 +1,52 @@
 
 from fastapi import APIRouter
 router = APIRouter()
-
+from pkg.aio_telegram_utils import get_profile_img_b64
+from pkg.telegram_utils import get_bot_data_by_token
+from db.mongodb import get_database
 
 
 @router.get("/test")
 async def test():
     print("HIHI")
     return "Hi Hi"
+
+
+from pydantic import BaseModel, Field
+
+class add_bot_format(BaseModel):
+    creator: str = "example@gmail.com"
+    bot_token: str = "1234567890:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+from datetime import datetime
+
+@router.post("/add_bot")
+async def addNewBot(data: add_bot_format):
+    bot_data = get_bot_data_by_token(data.bot_token)
+    if bot_data == None:
+        message = "Fail, Bot Not Found"
+    else:
+        out_data = {
+        "Token": data.bot_token,
+        "tg_username": f"@{bot_data['username']}",
+        "Creator": data.creator,
+        "Custom_Response": [], # S.M.A.R.T
+        "is_public": False,
+        "usage_count": 0,
+        "is_reciever": True,
+        "response_bots": [],
+        "last_update": datetime.now(),
+        "create_time": datetime.now(),
+        "report_list": [],
+        }
+        db = await get_database()
+        col = db["AI_Chatbot_Platform"]["bots"]
+
+        #確認是不是已經有了
+        already_have = await col.find_one({"Token": data.bot_token})
+        if already_have:
+            message = f"Fail, already have create by {already_have['Creator']} at {already_have['create_time']}"
+        else:
+            await col.insert_one(out_data)
+            message = "Success, Add Success."
+    return {"message": message}
--- app/pkg/telegram_utils.py
+++ app/pkg/telegram_utils.py
@@ -1,10 +1,10 @@
 from telegram import Bot
-from telegram.error import InvalidToken
+from telegram.error import InvalidToken, Unauthorized
 
 def get_bot_data_by_token(Token):
     try:  
         bot = Bot(Token)
         result = bot.get_me()
         return result.to_dict()
-    except InvalidToken:
+    except (InvalidToken, Unauthorized) as e:
         return None
