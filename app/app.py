b7afffef1cf814a6af1a3392f350f7ba65465a9c
EasonC13 <pricean01@gmail.com>
2021-03-29 07:13:43 +0800

    change main

--- app/app.py
+++ app/app.py
@@ -7,11 +7,16 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 #from .api.api_v1.api import router as api_router
-from core.config import ALLOWED_HOSTS, PROJECT_NAME, API_PORT
+from core.config import ALLOWED_HOSTS, PROJECT_NAME, API_PORT, API_V1_STR
 from core.errors import http_422_error_handler, http_error_handler
 from db.mongodb_connect import close_mongo_connection, connect_to_mongo
 from db.mongodb import AsyncIOMotorClient, get_database
 import asyncio
+
+
+# %%
+from pkg.aio_telegram_utils import get_profile_img_b64
+from pkg.telegram_utils import get_bot_data_by_token
 
 
 # %%
@@ -36,26 +41,26 @@ app.add_middleware(
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
 
+
 app.add_exception_handler(HTTPException, http_error_handler)
 app.add_exception_handler(HTTP_422_UNPROCESSABLE_ENTITY, http_422_error_handler)
 
 
 # %%
 
-#from api.api import router as api_router
-#app.include_router(api_router)
+from api.api_v1.api import router as api_v1_router
+app.include_router(api_v1_router, prefix=API_V1_STR)
 
 
 # %%
-from fastapi.staticfiles import StaticFiles
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 
 # %%
 from fastapi.responses import HTMLResponse
 @app.get("/")
 def home():
-    with open("./static/index.html") as f:
+    with open(f"{static_file_path}/index.html") as f:
         html = "".join(f.readlines())
     return HTMLResponse(content=html, status_code= 200)
 
@@ -71,6 +76,63 @@ def fun():
 # %%
 
 
+# %% [markdown]
+# from telegram import Bot
+# from telegram.error import InvalidToken
+# def get_bot_data_by_token(Token):
+#     try:  
+#         bot = Bot(Token)
+#         result = bot.get_me()
+#         return result.to_dict()
+#     except InvalidToken:
+#         return None
+
+# %%
+from datetime import datetime
+async def addNewBot(creator, bot_token):
+    bot_data = get_bot_data_by_token(bot_token)
+    if bot_data == None:
+        message = "Fail, Bot Not Found"
+    else:
+        data = {
+        "Token": bot_token,
+        "tg_username": f"@{bot_data['username']}",
+        "Creator": creator,
+        "Custom_Response": [], # S.M.A.R.T
+        "is_public": False,
+        "usage_count": 0,
+        "is_reciever": True,
+        "response_bots": [],
+        "last_update": datetime.now(),
+        "create_time": datetime.now(),
+        "report_list": [],
+        }
+        db = await get_database()
+        col = db["AI_Chatbot_Platform"]["bots"]
+
+        #確認是不是已經有了
+        already_have = await col.find_one({"Token": bot_token})
+        if already_have:
+            message = f"Fail, already have create by {already_have['Creator']} at {already_have['create_time']}"
+        else:
+            await col.insert_one(data)
+            message = "Success, Add Success."
+        return {"message": message}
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+static_file_path = "../front-end/dist"
+from fastapi.staticfiles import StaticFiles
+app.mount("/", StaticFiles(directory=static_file_path), name="static")
+
 
 # %%
 if __name__ == "__main__":
@@ -78,3 +140,25 @@ if __name__ == "__main__":
     nest_asyncio.apply()
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=API_PORT)
+
+# %% [markdown]
+# @app.post("/request_phone_code", tags=["user"])
+# def request_phone_code():
+#     pass
+# 
+# @app.post("/help_add_bot", tags=["user"])
+# def request_phone_code():
+#     pass
+# %% [markdown]
+# @app.get("/")
+# def index_page():
+#     pass
+
+# %%
+
+
+
+# %%
+
+
+
