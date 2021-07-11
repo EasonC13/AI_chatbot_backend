2973daa9239af7e2447ddccec0ab5d4fc2261768
EasonC13 <pricean01@gmail.com>
2021-07-11 10:25:52 +0800

    change start.sh

---
+++ app/start.sh
@@ -0,0 +1,4 @@
+eval "$(conda shell.bash hook)"
+conda activate ser
+cd /home/eason/Python/WEB/service/AI_Chatbot_platform/app
+uvicorn app:app --port 13525 --workers 1 --proxy-headers --host 0.0.0.0
